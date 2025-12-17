39ce22517dbdf871fb14a7d4fa01fea9259a8b41
lilniggafromdeo'block <ifranco@ctsformacion.com>
2025-12-17 13:24:28 +0100

    v1.3

---
+++ Assets/scripts/groundsensor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class grownsensor : MonoBehaviour
+{
+    public bool isGrounded;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+    
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+    
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
